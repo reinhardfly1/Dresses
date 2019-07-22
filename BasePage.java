18fd3ef87c5751b0110f9ae56e1a7569c98a7961
reinhardfly1 <53135187+reinhardfly1@users.noreply.github.com>
2019-07-23 00:44:57 +0530

    relative

--- BasePage.java
+++ BasePage.java
@@ -1,6 +1,6 @@
 package reinhard;
 import java.util.concurrent.TimeUnit;
-
+/relative/
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
