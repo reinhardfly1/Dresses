e320561c658269f74c01009c4552eece858dbe70
reinhardfly1 <53135187+reinhardfly1@users.noreply.github.com>
2019-07-23 11:33:35 +0530

    any
    rein

--- BasePage.java
+++ BasePage.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;hlr3qhgihqreoiigijg3h
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
